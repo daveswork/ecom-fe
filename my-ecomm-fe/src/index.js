fb1dc0b6d7bdf3845bc8806dc0ddfaddcd2b96a2
daveswork <29786887+daveswork@users.noreply.github.com>
2025-02-28 17:44:03 -0500

    Getting started with bootstrap.

--- my-ecomm-fe/src/index.js
+++ my-ecomm-fe/src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
-var element = <div>Hello World!</div>;
+var element = <button className="btn btn-danger">Hello World!</button>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
