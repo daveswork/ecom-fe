a9bc83f92a8b02f24afac3ddcce0e0baf4da5279
daveswork <29786887+daveswork@users.noreply.github.com>
2025-02-28 18:55:57 -0500

    Refactored NavBar, added MainComponent.

---
+++ my-ecomm-fe/src/MainContent.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class MainComponent extends Component {
+  render() {
+    return <div>Hello from Main Component</div>;
+  }
+}
--- my-ecomm-fe/src/index.js
+++ my-ecomm-fe/src/index.js
@@ -4,8 +4,8 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import NavBar from "./App";
+import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<NavBar />);
+root.render(<App />);
